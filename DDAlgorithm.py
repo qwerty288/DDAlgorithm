eb306714b9b8123dbe85d8482f1d162b63701779
qwerty288 <anon88e@gmail.com>
2021-11-01 12:56:54 +0000

    small improvements + removed visualization

--- DDAlgorithm.py
+++ DDAlgorithm.py
@@ -1,49 +1,38 @@
 import math
 import random
-
 import networkx as nx
-import matplotlib.pyplot as plt
 
-# n individuals, n^theta infected and m tests
-def DDAlgorithm(n,theta, m):
-    # initialize vars
-    infectedNo = round(math.pow(n, theta))
+
+def dd_algorithm(n, theta, m):
+    # initialize variables
+    infectedNo = math.pow(n, theta)
     delta = round((m * math.log(2)) / infectedNo)
+    infectedNo = round(infectedNo)
     positiveTests = set()
     positiveIdentifiedIndividuals = set()
-    individualsSet = set()
     G = nx.Graph()
     # generate bipartite graph matching individuals to tests
-    for x in range(m, m+n):
-        individualsSet.add(x)
+    for x in range(m, m + n):
         matchedTests = random.sample(range(0, m), delta)
         for i in matchedTests:
             G.add_edge(x, i)
-    # display generated bipartite graph
-    nx.draw_networkx(G, pos = nx.drawing.layout.bipartite_layout(G, individualsSet))
-    plt.show()
-    # save test results
-    for x in range(m, m+infectedNo):
+    # create test results (consider individuals m to m+infectedNo as infected and mark their connected tests as +ve)
+    for x in range(m, m + infectedNo):
         for i in list(G.neighbors(x)):
             positiveTests.add(i)
-    # remove individuals connected to a negative test
+    # remove individuals connected to a negative test from the graph
     for x in range(0, m):
         if x not in positiveTests and G.has_node(x):
             for i in list(G.neighbors(x)):
                 G.remove_node(i)
-    # find all individuals connected to 1 positive test
+    # find all positive tests connected to only 1 individual, and mark the individual
     for x in range(0, m):
         if x in positiveTests:
             connectedIndividuals = list(G.neighbors(x))
             if len(connectedIndividuals) == 1:
                 positiveIdentifiedIndividuals.add(connectedIndividuals[0])
-    # print set of found individuals
-    print(positiveIdentifiedIndividuals)
-
-DDAlgorithm(20, 0.5, 10)
+    # print length of set containing such individuals
+    print(len(positiveIdentifiedIndividuals))
 
 
-
-
-
-
+dd_algorithm(49, 0.5, 49)
